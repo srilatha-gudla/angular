45a792167e6c79211da65c0da66b221ef5a600e3
srilatha-gudla <srilathagudla55@gmail.com>
2024-02-23 18:04:13 +0530

    update

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'components-movie';
+  img1="https://b.zmtcdn.com/data/pictures/7/18356437/31925247a0141db017dbfa5d780c3cb5.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*";
+  img2 =
+    'https://occ-0-2040-2164.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABZMqt09-0DljMr8VhUC4ZKWYnc6sdTZZ--qKqUoMm3cx4GmOvu-Dzx8J1SDxrcPQl_zx6NT2x476QvEWy7CrkJowJbB32FNVzWn9xVS4k7OgO-E3Agyl2d2wGS5h87RnaBQPdPSQZXH_4qa-RkwgF9fa6p4gmaG0Bqc1xL6HbOcHOkl7MhGQdbSeCz00wJ8.jpg?r=088';
+  img3 =
+    'https://occ-0-2040-2164.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABZVmxSkUTWP6_xyLXe8InZ_Fkpe-Gw_lUk9z_mnKvB9ka6v0K0QtRLBkjLkLsN6di2uqWI9Dw5VZRxHPd0KFTxJAICwVRqoDn-mRZV-bHbb5oDSyAeO8okqNtxbGIYQLs7ri.jpg?r=883';
+  img4 =
+    'https://occ-0-2040-2164.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABX8EdjlxR-oKW1CgQn5eFGS_qaTLehs6_4ftvnU7WzViQXFKnR5fbU0LAvH-2ZEwQlWfOsxNNaG55miugeqFfU003VFL3rP-4a1_ltI98M8xm01FyWIiJ2VQ9CKi7i29l-86.jpg?r=152';
+  img5 =
+    'https://occ-0-2040-2164.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABXmLVtUXMDD6mDV5I6VfUyjH5bAoDPlzxqsR37MxELC_G-balPjtinRm3zTY33c7IlhFRH9UGSSJUTJX_pRmuLnx4PRMXbdlao7zDwzLvLlpwukI0m_sm5Av4nagWpQ0rik2.jpg?r=488';
+}
